9ea42700be8c337a229658c2a4c07d14bb63843f
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-01-31 17:11:40 +0900

    Timeline에 Follow하는 유저들의 포스팅 만을 노출하기

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
 from instagram.form import PostForm
@@ -10,11 +11,17 @@ from instagram.models import Tag, Post
 # Create your views here.
 
 def index(request):
+    post_list = Post.objects.all()\
+        .filter(
+        Q(author=request.user) |
+        Q(author__in=request.user.following_set.all())
+    )
     suggested_user_list = get_user_model().objects.all()\
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all())[:3]
 
     return render(request, 'instagram/index.html', {
+        'post_list': post_list,
         'suggested_user_list': suggested_user_list,
     })
 
