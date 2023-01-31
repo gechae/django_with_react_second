088a82551cdf95821503e43a732f8f4ff15c5fca
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-01-31 15:06:47 +0900

    유저 페이지 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.urls import path, re_path
 
 from instagram import views
 
 # include에 한함 - url reverse(perfix) 역할(namespace)
 app_name = 'instagram'
 
+# username_regex = UnicodeUsernameValidator.regex.lstrip('^').strip('$')
+# print(f'username_regex: {repr(username_regex)}')
+
 urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #re_path(r'(?P<username>[\w.@+-]+' + username_regex +')/', views.user_page, name='user_page'),
+    re_path(r'(?P<username>[\w.@+-]+)/$', views.user_page, name='user_page'),
 ]
