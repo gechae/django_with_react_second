1e9d8d1e971ec6caa9e2b1ee9c75476f4fcbadf2
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-02-01 10:59:56 +0900

    장고 만으로 댓글 쓰기 구현

--- instagram/views.py
+++ instagram/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
-from instagram.form import PostForm
+from instagram.form import PostForm, CommentForm
 from instagram.models import Tag, Post
 
 
@@ -89,4 +89,21 @@ def user_page(request, username):
         'post_list': post_list,
         'post_list_count': post_list_count,
         'is_follow': is_follow
+    })
+
+@login_required
+def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect(comment.post)
+    else:
+       form = CommentForm()
+    return render(request, "instagram/comment_form.html", {
+        "form": form,
     })
