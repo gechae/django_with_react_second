4aa9c3f64508dc2b64ea1af3cb08c4235ad96a45
채광은(IT서비스본부/개발실/개발5팀/프로) <cls@emforce.co.kr>
2023-02-01 11:47:46 +0900

    자바스크립트없이 포스팅 화면에서 댓글 쓰기 UI 개선

--- instagram/views.py
+++ instagram/views.py
@@ -25,9 +25,11 @@ def index(request):
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all())[:3]
 
+    comment_form = CommentForm
     return render(request, 'instagram/index.html', {
         'post_list': post_list,
         'suggested_user_list': suggested_user_list,
+        'comment_form': comment_form,
     })
 
 @login_required
@@ -51,8 +53,10 @@ def post_new(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    comment_form = CommentForm()
     return render(request, "instagram/post_detail.html", {
-        "post": post
+        "post": post,
+        'comment_form': comment_form,
     })
 
 
